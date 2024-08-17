97eea346f3cada11a771a031b92d64d2badadf79
LeeJangBok <wkdqhr73@gkes.co.kr>
2024-08-17 16:58:23 +0900

    메인 타이틀 화면 (옵션제작중1)
    ㅇㅇ

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
+
+    public GameObject optionsPanel;  
+
     public void StartGame()
     {
         
@@ -19,5 +22,11 @@ public class Title : MonoBehaviour
 
         //나중에 어플리케이션에서의 게임종료 (코드잠군상태)
        // Application.Quit();
+    }
+
+    public void ToggleOptionsPanel()
+    {
+       
+        optionsPanel.SetActive(!optionsPanel.activeSelf);
     }
 }
