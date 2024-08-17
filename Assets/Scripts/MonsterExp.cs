88b1722549a9815a909b8471631a0f65526e16a5
swp05 <swp05@naver.com>
2024-08-17 16:05:09 +0900

    [swp05] monster/Boss monster Exp

--- Assets/Scripts/MonsterExp.cs
+++ Assets/Scripts/MonsterExp.cs
@@ -8,14 +8,14 @@ public class MonsterExp : ScriptableObject
 {
     //¾Æ·¡·Î °¥¼ö·Ï °­ÇØÁü
     public int a = 1;
-    public int FlyingEye = 3;
+    public int FlyingEye = 3; //º¸½º ¸÷
     public int Goblin = 4;
     public int Mushroom = 5;
     public int Skeleton = 5;
     public int EvilWizard1 = 15;
-    public int EvilWizard2 = 18;
+    public int EvilWizard2 = 18; //º¸½º ¸÷
     public int EvilWizard3 = 25;
     public int HeroKnight1 = 35;
     public int HeroKnight2 = 50;
-    public int MartialHero = 55;
+    public int MartialHero = 55; // º¸½º ¸÷
 }
