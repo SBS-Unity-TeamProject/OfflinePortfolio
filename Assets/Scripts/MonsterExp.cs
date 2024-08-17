f21eb503693a4d6eb85e1cac719d43cabcdfc47a
swp05 <swp05@naver.com>
2024-08-17 15:44:40 +0900

    [swp05] Monster Health Settings

--- Assets/Scripts/MonsterExp.cs
+++ Assets/Scripts/MonsterExp.cs
@@ -6,15 +6,16 @@ using UnityEngine;
 [CreateAssetMenu (fileName = "MonsterExp", menuName = "MonsterStatus/Exp", order = 2)]
 public class MonsterExp : ScriptableObject
 {
+    //아래로 갈수록 강해짐
     public int a = 1;
     public int FlyingEye = 3;
-    public int Goblin = 5;
+    public int Goblin = 4;
     public int Mushroom = 5;
     public int Skeleton = 5;
-    public int EvilWizard1;
-    public int EvilWizard2;
-    public int EvilWizard3;
-    public int HeroKnight1;
-    public int HeroKnight2;
-    public int MartialHero;
+    public int EvilWizard1 = 15;
+    public int EvilWizard2 = 18;
+    public int EvilWizard3 = 25;
+    public int HeroKnight1 = 35;
+    public int HeroKnight2 = 50;
+    public int MartialHero = 55;
 }
