b6424363372090a757c21832aac5792753605a21
swp05 <swp05@naver.com>
2024-08-24 14:44:55 +0900

    [swp05] Main Scene Monsters

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -7,7 +7,7 @@ public class Arrow : MonoBehaviour
 {
     [SerializeField] PlayerStates playerStates;
     public int damage = 10;
-    public Scanner scanner;
+    [SerializeField] Scanner scanner;
     //public WeaponLauncher weaponLauncher;
     //public Transform arrowpos;
     //public Vector2 dir = Vector2.zero;
